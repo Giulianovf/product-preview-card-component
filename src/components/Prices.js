2f461f171b1d0668d828053bda4462d722c8dcb1
Giulianovf <giulianovf@hotmail.com>
2023-02-22 12:41:32 -0300

    incremento root

--- src/components/Prices.js
+++ src/components/Prices.js
@@ -4,7 +4,11 @@ function Prices(){
     return (
         <div className={styles.Prices}>
             <h2 className="item">$149.99</h2>
-            <p className="item">$169.99</p>
+            <span className='item line'>
+                <p className="item">
+                $169.99
+                </p>
+            </span>
         </div>
     )
 }
